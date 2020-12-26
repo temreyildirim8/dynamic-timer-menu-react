ba9680b357b70d005aecbfe3b62cd08ecd402a8b
Emre <temreyildirim8@gmail.com>
2020-12-26 17:10:05 +0300

    Added animations

--- src/pages/Step2.tsx
+++ src/pages/Step2.tsx
@@ -1,10 +1,17 @@
 import React, { FC  } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 import step2 from '../assets/images/step2.png';
 
+const spin = keyframes`
+  from { transform: translate( 0%, 0% ) rotate(0turn); }
+  to { transform: translate( 0%, 0% ) rotate(1turn); }
+`;
+
 const StyledDiv = styled.div`
+  animation: ${spin} 1s 1;
 `
+
 const StyledImg = styled.img`
   height: 500px;
 `
